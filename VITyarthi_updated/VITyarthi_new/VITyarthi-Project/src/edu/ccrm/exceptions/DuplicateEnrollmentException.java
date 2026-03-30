5f98d91bc57efbb7e1773e8e1d58f9af60673423
Jai Dhuria <jaidhuria002@gmail.com>
2026-03-30 23:43:38 +0530

    Delete VITyarthi_updated/VITyarthi_new/VITyarthi-Project directory

--- VITyarthi_updated/VITyarthi_new/VITyarthi-Project/src/edu/ccrm/exceptions/DuplicateEnrollmentException.java
+++
@@ -1,7 +0,0 @@
-package edu.ccrm.exceptions;
-
-public class DuplicateEnrollmentException extends Exception {
-    public DuplicateEnrollmentException(String message) {
-        super(message);
-    }
-}
--- VITyarthi_updated/VITyarthi_new/VITyarthi-Project/src/edu/ccrm/util/Validators.java
+++
@@ -1,7 +0,0 @@
-package edu.ccrm.util;
-
-public class Validators {
-    public static boolean isEmailValid(String s) {
-        return s != null && s.contains("@") && s.contains(".");
-    }
-}
